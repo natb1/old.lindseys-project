8fd6090766b729d67bb5c2f2bcdfe1169178cc4f
Nathan Buesgens <nathan@natb1.com>
2014-12-18 05:57:00 +0000

    badic layout of new domain modal

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,8 +7,8 @@ controllers.controller('NavCtrl', ['$scope', 'alerts',
   }
 ]);
 
-controllers.controller('DomainCtrl', ['$scope', 'Domains', 'Studies', 'alerts',
-  function ($scope, Domains, Studies) {
+controllers.controller('DomainCtrl', ['$scope', '$modal', 'Domains', 'Studies', 'alerts',
+  function ($scope, $modal, Domains, Studies, alerts) {
 
     $scope.domains = Domains.get()
     $scope.studies = {}
@@ -35,7 +35,46 @@ controllers.controller('DomainCtrl', ['$scope', 'Domains', 'Studies', 'alerts',
       )
     }
 
-    $scope.new_domain = function(){}
+    $scope.new_domain = function(){
+
+      $scope.loading = true
+
+      var modalInstance = $modal.open({
+        templateUrl: 'partials/domain-new.html',
+        controller: 'DomainNewCtrl',
+        resolve: {
+            domains: function(){ return $scope.domains }
+        }
+      })
+
+      modalInstance.result.then(function(new_domain){
+        $scope.domains = Domains.get(function(){
+          $scope.loading = false
+          if(new_domain){
+            $scope.select_domain(new_domain)
+          }
+        })
+      });
+    }
 
   }
 ]);
+
+controllers.controller('DomainNewCtrl', ['$scope', '$modalInstance', 'alerts', 'domains',
+  function($scope, $modalInstance, alerts, domains){
+
+    $scope.domains = domains
+
+    $scope.new_domain = function(name){
+      console.log('new domain '+name)
+    }
+
+    $scope.ok = function(){
+      $modalInstance.close();
+    }
+
+    $scope.cancel = function(){
+      $modalInstance.dismiss('cancel')
+    }
+  }
+])
