1b486c18a61447ad4620d37fd07c6a053d6a0ec3
Nathan Buesgens <nathan@natb1.com>
2014-12-18 00:53:24 +0000

    adds failing test for list studies in measure

--- test/e2e/analyst/information about studies that use measures in a domain.js
+++ test/e2e/analyst/information about studies that use measures in a domain.js
@@ -6,7 +6,7 @@ var utils = require('../../utils')
 
 describe("Lindsey's Project", function(){
 
-  beforeEach(function() {
+  beforeEach(function(){
     browser.get('')
   });
 
@@ -23,37 +23,66 @@ describe("Lindsey's Project", function(){
     }
   }
 
-  var domain_elements = element.all(
-    by.repeater('(domain_name, measures) in domains'))
-  var measure_elements = element.all(
-    by.repeater('measure in domains[selected_domain]'))
+  var _test_studies_in_measure = function(measure, domain){
+    it('lists each study in measure '+measure, function(){
+      element(by.linkText(domain)).click()
+      _find_measure_group(measure, function(measure_group){
+        measure_group.all(
+          by.repeater('(study_name, study) in studies[measure]')
+        ).map(function(study_row){
+          return study_row.element(by.binding('study_name')).getInnerHtml()
+        }).then(function(study_names){
+          expect(study_names.sort()).toEqual(studies_with[measure].sort())
+        })
+      })
+    })
+  }
 
-  it('enumerates all domains for selection', function(){
-    domain_elements.map(function(tab_element){
-      var tab_name = tab_element.element(by.binding('domain_name')).getText()
-      return tab_name
-    }).then(function(domain_tab_names){
+  var _find_measure_group = function(measure, callback){
+    element.all(by.repeater('measure in domains[selected_domain]')).each(
+      function(measure_group){
+        measure_group
+          .element(by.css('.panel-title'))
+          .element(by.binding('measure'))
+          .getText()
+          .then(function(measure_name){
+            if (measure_name == measure){
+              callback(measure_group)
+            }
+          })
+       }
+     )
+  }
+
+  it('lists all domains', function(){
+    element.all(by.repeater('(domain_name, measures) in domains')).map(
+      function(tab_element){
+        return tab_element.element(by.binding('domain_name')).getText()
+      }
+    ).then(function(domain_tab_names){
       expect(domain_tab_names.sort()).toEqual(Object.keys(domains).sort())
     })
   })
 
-  it('lists all measures in a selected domain', function(){
-    domain_elements.each(function(tab_element){
-      var domain_name_element = tab_element.element(by.binding('domain_name'))
-      domain_name_element.click()
-      domain_name_element.getText().then(function(domain_name){
-        measure_elements.map(function(measure_group){
-          var measure_nm = measure_group.element(by.binding('measure')).getText()
-          return measure_nm
-        }).then(function(measure_names){
-          expect(measure_names.sort()).toEqual(domains[domain_name].sort())
-        })
-      })
-    });
-  });
+  for (var domain in domains){
 
-  it('lists all studies in a selected measure', function(){
-    //expect each measure to have correct list of studies
-  });
+    it('lists each measure in domain '+domain, function(){
+      element(by.linkText(domain)).click().then(function(){})
+      element.all(by.repeater('measure in domains[selected_domain]')).map(
+        function(measure_group){
+          return measure_group
+            .element(by.css('.panel-title'))
+            .element(by.binding('measure'))
+            .getText()
+        }
+      ).then(function(measure_names){
+        expect(measure_names.sort()).toEqual(domains[domain].sort())
+      });
+    })
+
+    domains[domain].forEach(function(measure){
+      _test_studies_in_measure(measure, domain)
+    });
+  }
 
 });
